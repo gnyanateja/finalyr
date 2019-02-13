51fa7e34bbb7d9402b20ec397843d0a69f2fafec
Gnyana teja <cb.en.u4cse16247@cb.students.amrita.edu>
2019-02-14 03:40:54 +0530

    first try

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.send('hello'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
