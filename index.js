5f9f06c0c41bab3fabe359eeecf838ef21eb3211
Gnyana teja <cb.en.u4cse16247@cb.students.amrita.edu>
2019-02-28 16:08:26 +0530

    make it better

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const PORT = process.env.PORT || 5000
 var usersRouter = require('./routes/app');
 
+var cors=require('cors');
+app.use(cors({
+    origin:['http://localhost:4200']
+  }));
+
 // add mongoose
  var mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://narutoteja:teja@cluster0-0q64p.mongodb.net/pmail?retryWrites=true',{useNewUrlParser: true});
