83f8379372a3581e1aaba903f0f6df5812975beb
Gnyana teja <cb.en.u4cse16247@cb.students.amrita.edu>
2019-03-17 06:59:29 +0530

    make it better

--- routes/app.js
+++ routes/app.js
@@ -168,7 +168,7 @@ var time = hh + ":" + mm + ":" + ss;
               if (err){
                   console.log(err);  // returns error if no matching object found
               }else{
-                  res.send({"message":t});
+                  res.send({"message":req.body.choice});
               }
             }
           )
